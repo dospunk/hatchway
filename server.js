49bebd3a6d05e68bed0cb08906759135968e1aaa
dospunk <lostboycmd@gmail.com>
2020-02-08 13:52:41 -0500

    more mysql

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const codeGen = require('./lib/gencode');
 const mysql = require('mysql');
+const mysqlConf = require("./lib/mysqlconf");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mysqlLogin = {
-	host: process.env.SQLIP,
-	//port: '3306',
-	user: 'root',
-	password: process.env.SQLPASS,
-	database: 'vredudb',
-	ssl: {
-		ca: process.env.SQLSERVERCA,
-		key: process.env.SQLCLIENTKEY,
-		cert: process.env.SQLCLIENTCERT
-	}
-}
+const mysqlLogin = mysqlConf.mysqlLogin;
 
 app.use(express.static('public'));
 
