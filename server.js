9784c228066db6c906ee9e9e3ccd07c8d76678be
dospunk <lostboycmd@gmail.com>
2020-02-08 14:19:59 -0500

    add envlist api endpoint

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ const mysqlLogin = mysqlConf.mysqlLogin;
 
 app.use(express.static('public'));
 
-app.get('/code', (req, res) => {
+app.post('/code', (req, res) => {
 	res.send(codeGen.generateCode());
+});
+
+app.post('/envlist', (req, res) => {
+	const connection = mysql.createConnection(mysqlLogin);
+	connection.connect();
+	connection.query('SELECT * from envs;', (error, results, fields) => {
+		if(error) throw error;
+		res.json(results);
+	});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
