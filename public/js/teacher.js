98e8fbf8a608e1eb0c378b0171e28795b9eaacae
dospunk <lostboycmd@gmail.com>
2020-02-08 23:21:25 -0500

    move class back cause it messed up the image

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -2,9 +2,9 @@ const radioName = "env";
 
 function buildEnvListEntry(data){
 	return `
-	<div>
+	<div class="envOption">
 		<input type="radio" name="${radioName}" value="${data.envId}" id="${data.name+data.envId}"> 
-		<label for="${data.name+data.envId}" class="envOption">
+		<label for="${data.name+data.envId}">
 			<img src="/img/${data.imgPath}" width="100" height="100">
 			<h2>${data.name}</h2>
 			<p class="desc">${data.description}</p>
