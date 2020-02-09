568b286a0f38c837f24ded9a6e6bfca299056128
dospunk <lostboycmd@gmail.com>
2020-02-08 22:19:15 -0500

    flexbox baybeeeeeee

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -2,7 +2,7 @@ const radioName = "env";
 
 function buildEnvListEntry(data){
 	return `
-	<div>
+	<div class="envOption">
 		<input type="radio" name="${radioName}" value="${data.envId}" id="${data.name+data.envId}"> 
 		<label for="${data.name+data.envId}">
 			<img src="/img/${data.imgPath}" width="100" height="100"> ${data.name} 
