f10d0309522f331479e3726d218678c4824c2e45
dospunk <lostboycmd@gmail.com>
2020-02-08 21:36:53 -0500

    dumb

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,7 +1,7 @@
 const radioName = "env";
 
 function buildEnvListEntry(data){
-	return `<div><input type="radio" name="${radioName}" value="${data.envId}> ${data.name} <br> <span class="desc"> ${data.description} </span> </div>`;
+	return `<div><input type="radio" name="${radioName}" value="${data.envId}"> ${data.name} <br> <span class="desc"> ${data.description} </span> </div>`;
 }
 
 
