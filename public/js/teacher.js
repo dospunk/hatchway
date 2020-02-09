19d65295e12eaecdba30b9c4fa5a0a412aff67cf
dospunk <lostboycmd@gmail.com>
2020-02-08 21:39:36 -0500

    radio button label tag

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,7 +1,13 @@
 const radioName = "env";
 
 function buildEnvListEntry(data){
-	return `<div><input type="radio" name="${radioName}" value="${data.envId}"> ${data.name} <br> <span class="desc"> ${data.description} </span> </div>`;
+	return `
+	<div>
+		<label>
+			<input type="radio" name="${radioName}" value="${data.envId}"> ${data.name} 
+		</label>
+		<span class="desc"> ${data.description} </span>
+	</div>`;
 }
 
 
