80500c0dbb980a0da09f2cc43516a0508d9eb128
dospunk <lostboycmd@gmail.com>
2020-02-08 22:08:00 -0500

    Update teacher.js

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -3,8 +3,8 @@ const radioName = "env";
 function buildEnvListEntry(data){
 	return `
 	<div>
-		<label>
-			<input type="radio" name="${radioName}" value="${data.envId}"> 
+		<input type="radio" name="${radioName}" value="${data.envId}" id="${data.name+data.envId}"> 
+		<label for="${data.name+data.envId}">
 			<img src="/img/${data.imgPath}" width="100" height="100"> ${data.name} 
 		</label>
 		<span class="desc"> ${data.description} </span>
