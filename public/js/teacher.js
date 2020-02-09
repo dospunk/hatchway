245d3f01599a577b28b8286f121c897dca9df975
dospunk <lostboycmd@gmail.com>
2020-02-08 21:27:16 -0500

    build envs list

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,5 +1,14 @@
+const radioName = "env";
+
+function buildEnvListEntry(data){
+	return `<div><input type="radio" name="${radioName}" value="${data.envId}> ${data.name} <br> <span class="desc"> ${data.desc} </span> </div>`;
+}
+
+
 $.post("/envlist", {}, (data)=>{
-	console.log(data);
+	$.each(data, (idx, elem)=>{
+		$("#envs").append(buildEnvListEntry(elem));
+	});
 }).fail((jqXHR, status, err)=>{
 	console.error(status);
 	throw err;
